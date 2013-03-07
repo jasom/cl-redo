90fd8735088635cecbbf7a294dd4c4f5f03b35cb
Jason Miller <aidenn0@geocities.com>
2013-03-06 20:06:02 -0800

    Stop using pathnames

--- redo-fs.lisp
+++ redo-fs.lisp
@@ -16,8 +16,15 @@
 	   (dbfile
 	    (merge-file-paths dbfile *redo-dbpath*)))
       (when (iolib/os:file-exists-p dbfile)
-	(with-open-file (f (file-path-namestring dbfile))
-	  (read f))))))
+	(read-from-string
+	 (let ((size (1+ (isys:stat-size
+			  (isys:stat (file-path-namestring dbfile))))))
+	   (cffi:with-foreign-pointer-as-string (buf size)
+	     (let ((fd (isys:open (file-path-namestring dbfile) isys:O-RDONLY)))
+	       (unwind-protect
+		    (isys:read fd buf (1- size))
+		 (isys:close fd)))
+	     (setf (cffi:mem-aref buf :char (1- size)) 0))))))))
 
 
 (defmethod (setf %db-file-info) (value (type (eql :fs)) file)
@@ -32,8 +39,15 @@
 	    (merge-file-paths dbfile *redo-dbpath*)))
       (ensure-file-path-directory-exists dbfile #o755)
       ;TODO fix for paths that aren't valid lisp paths
-      (with-open-file (f (file-path-namestring dbfile) :direction :output :if-exists :supersede)
-	(prin1 value f)))))
+      (let
+	  ((octets (babel:string-to-octets (prin1-to-string value))))
+      (cffi:with-pointer-to-vector-data (buf octets)
+	(let ((fd (isys:open (file-path-namestring dbfile)
+			     (logior isys:O-TRUNC isys:O-CREAT isys:O-WRONLY))))
+	      (unwind-protect
+		 (isys:write fd buf (length octets))
+		(isys:close fd))))))))
+      
 
 (defmethod %makunbound-db-file-info ((db-type (eql :fs)) file)
   (let ((file (file-path file)))
